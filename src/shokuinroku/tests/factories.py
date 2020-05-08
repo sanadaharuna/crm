ff66361d3c52153f20e00b0f8a3e0e816f36977b
hiraikatsuyuki <hirai@adm.niigata-u.ac.jp>
2020-05-08 13:42:16 +0900

    setup shokuinroku test

---
+++ src/shokuinroku/tests/factories.py
@@ -0,0 +1,19 @@
+import factory
+import factory.fuzzy
+import factory.django
+from shokuinroku.models import Shokuin
+
+
+class ShokuinFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Shokuin
+
+    as_of = factory.Faker("date")
+    shokuin_id = factory.Sequence(lambda n: "%08d" % n)
+    shozokumei = factory.Faker("prefecture", locale="ja_jp")
+    kakarikouzamei = factory.Faker("city", locale="ja_jp")
+    shokumei = factory.Faker("town", locale="ja_jp")
+    kanjishimei = factory("name", locale="ja_jp")
+    furigana = factory("kana_name", locale="ja_jp")
+    naisenbangou = factory.Sequence(lambda n: "%04d" % n)
+    mail_address = factory.ascii_safe_email()
