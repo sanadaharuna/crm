e41c74ea575d15448a0a7db55ddde7f963a438d7
hiraikatsuyuki <hirai@adm.niigata-u.ac.jp>
2020-05-17 04:43:51 +0900

    modify for django-filter

--- src/nayose/filters.py
+++ src/nayose/filters.py
@@ -3,5 +3,8 @@ from nayose.models import Nayose
 
 
 class NayoseFilter(django_filters.FilterSet):
+    # nayose_id = django_filters.NumberFilter()
+
     class Meta:
         model = Nayose
+        fields = ["nayose_id", "shokuin_id"]
