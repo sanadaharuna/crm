aa2d6ef77c16b57da714dfeed4297e50e87dcf70
hiraikatsuyuki <hirai@adm.niigata-u.ac.jp>
2020-05-19 02:14:52 +0900

    prepare filter and pagination

--- src/nayose/urls.py
+++ src/nayose/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
-# from django_filters.views import FilterView
 
 from nayose import views
-# from nayose.models import Nayose
 
 app_name = "nayose"
 urlpatterns = [
     path("", views.NayoseFrontView.as_view(), name="front"),
-    # path("list", views.NayoseListView.as_view(), name="list"),
-    path("list", views.NayoseFilterView.as_view(), name="list"),
+    path("list", views.NayoseListView.as_view(), name="list"),
     path("create", views.NayoseCreateView.as_view(), name="create"),
     path("<int:pk>/detail", views.NayoseDetailView.as_view(), name="detail"),
     path("<int:pk>/update", views.NayoseUpdateView.as_view(), name="update"),
