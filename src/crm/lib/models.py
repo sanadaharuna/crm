6ece6f8487841e043eac7429590986ede0b1af57
katsuyuki hirai <hirai@adm.niigata-u.ac.jp>
2021-01-26 13:17:14 +0900

    add shokui to Person model

--- src/crm/lib/models.py
+++ src/crm/lib/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class Person(models.Model):
     eradcode = models.CharField("研究者番号", max_length=8, blank=True, null=True)
-    shimei = models.CharField("氏名", max_length=50)
-    shozoku = models.CharField("所属", max_length=50)
+    shimei = models.CharField("氏名", max_length=50, blank=True, null=True)
+    shozoku = models.CharField("所属", max_length=50, blank=True, null=True)
+    shokui = models.CharField("職位", max_length=50, blank=True, null=True)
 
     class Meta:
         abstract = True
